5344470953c47194963874ab864679cba4594b87
Peter Frank <peter.frank@unfinished.com>
2022-10-06 14:56:10 -0700

    Using Bob since he's not aura

--- rpc/rpc-api/src/test/java/com/strategyobject/substrateclient/rpc/api/section/AuthorTests.java
+++ rpc/rpc-api/src/test/java/com/strategyobject/substrateclient/rpc/api/section/AuthorTests.java
@@ -51,13 +51,13 @@ class AuthorTests {
             val author = TestsHelper.createSectionFactory(wsProvider).create(Author.class);
 
             val publicKey = PublicKey.fromBytes(
-                    HexConverter.toBytes("0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"));
+                    HexConverter.toBytes("0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48"));
             val keyType = "aura";
             var result = author.hasKey(publicKey, keyType).get(WAIT_TIMEOUT, TimeUnit.SECONDS);
 
             assertFalse(result);
 
-            author.insertKey(keyType, "alice", publicKey).get(WAIT_TIMEOUT, TimeUnit.SECONDS);
+            author.insertKey(keyType, "bob", publicKey).get(WAIT_TIMEOUT, TimeUnit.SECONDS);
             result = author.hasKey(publicKey, keyType).get(WAIT_TIMEOUT, TimeUnit.SECONDS);
 
             assertTrue(result);
