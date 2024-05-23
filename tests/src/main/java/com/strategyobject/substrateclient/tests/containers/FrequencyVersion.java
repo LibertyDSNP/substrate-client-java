ab7907e14d92638244c64181de185fe88f1c0073
Peter Frank <peter.frank@unfinished.com>
2024-05-23 14:03:15 -0700

    Now using Frequency 1.12.0-rc3

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,8 +1,8 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.11.0";
-    public static final Long SPEC = 74L;
+    public static final String CURRENT_VERSION = "v1.12.0-rc3";
+    public static final Long SPEC = 78L;
 
     private FrequencyVersion() {
     }
